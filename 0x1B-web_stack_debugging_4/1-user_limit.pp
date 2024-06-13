53423971b4c3ff05fdb39d820c2d4e37ef501ef5
userkw <elkis.kawthar11@gmail.com>
2024-06-13 15:59:02 -0700

    done

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# change the os conf
+# user
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
